2c47b973fbb3c94f2fb30669f338ed4ddc059ba9
yudi.hardiyanta <yudi.hardiyanta@bps.go.id>
2026-05-05 08:13:25 +0800

    tuning order ranking

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get(`/api/search`, (req, res) => {
         //}
 
         if (lat != '' && long != '') {
-            sql += ` ORDER BY (1/distance*10 + score) DESC LIMIT 10`;
+            sql += ` ORDER BY (EXP(-distance) * 5 + score) DESC LIMIT 10`;
         }else{
             sql += ` ORDER BY score DESC LIMIT 10`;
         }
