29cf169eba4a670543bbe2cf51518787407281d0
yudi.hardiyanta <yudi.hardiyanta@bps.go.id>
2026-05-05 08:17:31 +0800

    update lagi

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get(`/api/search`, (req, res) => {
         //}
 
         if (lat != '' && long != '') {
-            sql += ` ORDER BY (EXP(-distance) * 5 + score*3) DESC LIMIT 10`;
+            sql += ` ORDER BY distance ASC, score DESC LIMIT 10`;
         }else{
             sql += ` ORDER BY score DESC LIMIT 10`;
         }
